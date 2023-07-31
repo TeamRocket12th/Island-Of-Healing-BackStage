8ce4f087b41867be750d4557a37e4316021df030
woowooyong <woowooyong@gmail.com>
2023-07-31 23:00:30 +0800

    feat: 預設一進入頁面會取2023年全年

--- src/components/HotWord.vue
+++ src/components/HotWord.vue
@@ -1,18 +1,16 @@
 <script setup lang="ts">
-import {  ref,onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 const apiBase = import.meta.env.VITE_API_URL
 interface ApiResponse {
   [key: string]: any
 }
-const datas = ref<ApiResponse[]>([]);
+const datas = ref<ApiResponse[]>([])
 // 取得熱門關鍵字
-const load = ref(true)
 
-
-const years = [2023,2024,2025,2026,2027,2028]
-const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,];
-const selectedYear = ref(null);
-const selectedMonth = ref('整年'); 
+const years = [2023, 2024, 2025, 2026, 2027, 2028]
+const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+const selectedYear = ref('2023')
+const selectedMonth = ref(0)
 
 const getHotWord = async () => {
   const token = localStorage.getItem('token')
@@ -20,16 +18,16 @@ const getHotWord = async () => {
     return
   }
   try {
-    const res: ApiResponse = await fetch(`${apiBase}/trendanalysis/get?year=${selectedYear.value}&month=${selectedMonth.value}`, {
-      headers: { 'Content-type': 'application/json', Authorization: `Bearer ${token}` }
-    })
+    const res: ApiResponse = await fetch(
+      `${apiBase}/trendanalysis/get?year=${selectedYear.value}&month=${selectedMonth.value}`,
+      {
+        headers: { 'Content-type': 'application/json', Authorization: `Bearer ${token}` }
+      }
+    )
     const data = await res.json()
     console.log(data)
     if (data.StatusCode === 200) {
-      load.value = false
-      console.log(data.sortedReturnResult)
       datas.value = data.sortedReturnResult
-
     } else {
       throw new Error(`發生錯誤 ${data.Message}`)
     }
@@ -45,57 +43,56 @@ onMounted(getHotWord)
     <div class="flex p-4 gap-4 items-center h-[40px]">
       <p>請選擇年/月</p>
       <select v-model="selectedYear" class="rounded p-2 h-[40px] w-[120px] cursor-pointer">
-        <option  v-for="year in years" :key="year" :value="year">{{ year }}</option>
+        <option v-for="year in years" :key="year" :value="year">{{ year }}</option>
       </select>
-      <select v-model="selectedMonth" class="rounded p-2 h-[40px]  w-[120px] cursor-pointer">
-        <option  selected   value="0" >整年</option>
-        <option  v-for="month in months" :key="month" :value="month">{{ month }}</option>
+      <select v-model="selectedMonth" class="rounded p-2 h-[40px] w-[120px] cursor-pointer">
+        <option selected value="0">整年</option>
+        <option v-for="month in months" :key="month" :value="month">{{ month }}</option>
       </select>
-      <button class="border h-[40px] px-4 bg-blue-900 text-white rounded" @click="getHotWord">查詢</button>
-
+      <button class="border h-[40px] px-4 bg-blue-900 text-white rounded" @click="getHotWord">
+        查詢
+      </button>
     </div>
   </div>
-    <div  class="flex flex-col">
-      <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-            <table class="min-w-full divide-y divide-gray-200">
-              <thead class="bg-gray-50">
-                <tr>
-                  <th
-                    scope="col"
-                    class="px-6 py-3 text-left font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    熱門關鍵字
-                  </th>
-                  <th
-                    scope="col"
-                    class="px-6 py-3 text-left font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    搜尋次數
-                  </th>
-
-                </tr>
-              </thead>
-              <tbody class="bg-white divide-y divide-gray-200" v-for="(data, index) in datas" :key="index">
-               
-                <tr >
-                  <td class="px-6 py-4 whitespace-nowrap text-gray-500">
-                    {{ data.Key }}
-                  </td>
-                  <td class="px-6 py-4 whitespace-nowrap text-gray-500">
-                    {{ data.Value }}
-                  </td>
-                  
-
-                </tr>
-              </tbody>
-            </table>
-          </div>
+  <div class="flex flex-col">
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+          <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+              <tr>
+                <th
+                  scope="col"
+                  class="px-6 py-3 text-left font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  熱門關鍵字
+                </th>
+                <th
+                  scope="col"
+                  class="px-6 py-3 text-left font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  搜尋次數
+                </th>
+              </tr>
+            </thead>
+            <tbody
+              class="bg-white divide-y divide-gray-200"
+              v-for="(data, index) in datas"
+              :key="index"
+            >
+              <tr>
+                <td class="px-6 py-4 whitespace-nowrap text-gray-500">
+                  {{ data.Key }}
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap text-gray-500">
+                  {{ data.Value }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
- 
-
+  </div>
 </template>
 <style></style>
